1deadd25533a63aad3a9634e4ff8eb5249c7f8b9
Li Dong <dongli@lasg.iap.ac.cn>
2020-03-22 22:14:11 +0800

    Use git clone instead of curl

--- .github/actions/deploy-jekyll-gh-pages/entrypoint.sh
+++ .github/actions/deploy-jekyll-gh-pages/entrypoint.sh
@@ -2,17 +2,17 @@
 # Exit immediately if a pipeline returns a non-zero status.
 set -e
 
+REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
 echo "🚀 Starting build action"
 
 mkdir /var/www && cd /var/www
 
-curl https://codeload.github.com/LASG-GAMIL/blog/zip/master -o blog.zip
-
-unzip blog.zip && rm blog.zip
+git clone $REPO repo
 
 echo "⚡️ Installing dependencies..."
 
-cd blog-master
+cd repo
 bundle install
 
 echo "🏋️ Building website..."
@@ -30,11 +30,10 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "GitHub Actions - $(date)"
-REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git push --force $REPO master:gh-pages
 
 rm -rf .git
 cd ../..
-rm -rf blog-master
+rm -rf repo
 
 echo "🎉 New version deployed 🎊"
